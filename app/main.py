c79b5e024bcd0d747fd6120c2a15b49b65199ced
henlam <henry.lam.1@stonybrook.edu>
2024-06-01 02:41:38 -0400

    refractor code

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
 import socket
 
 def handle_request(client_socket: socket):
+    # Receive 1024 bytes from the client
     client_socket.recv(1024)
 
+    # Send the encoded response to the client
     response = "HTTP/1.1 200 OK\r\n\r\n"
     client_socket.send(response.encode())
 
@@ -10,9 +12,11 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
+    # Create server socket and connect to clients
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client, addr = server_socket.accept() # wait for client
     
+    # Handle request and close connection
     handle_request(client)
     client.close()
 
